d585abf158a74336070fa17dc354e26b434424e1
Lucas Fragalá <lucfragala@gmail.com>
2023-09-25 11:42:11 -0300

    Initial commit

---
+++ aula01/index.php
@@ -0,0 +1,27 @@
+ORIENTAÇÃO A OBJETOS NO PHP OU OOP
+
+Objetos, são classes que criamos e nelas contém atributos e funções;
+Ex: classe Pessoa 
+Pode conter atributos como: Nome, Idade, Peso
+E Funções como: Crescer(), Comer(), Dormir()
+
+<?php 
+    class Pessoa{
+        //Objeto Pessoa
+
+        private $nome = 'Lucas';
+        private $idade = '28';
+        private $peso = '70 Kg';
+
+        private function crescer() {
+
+
+        }
+
+        private function comer() {
+
+            
+        }
+    }
+    
+?>
