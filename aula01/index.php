317a0a02ab4a66f57eefb3e7ba60250efe3c6e9d
Lucas Fragalá <lucfragala@gmail.com>
2023-09-25 11:59:47 -0300

    aula01

--- aula01/index.php
+++ aula01/index.php
@@ -1,9 +1,20 @@
-ORIENTAÇÃO A OBJETOS NO PHP OU OOP
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 01 - PHP</title>
+</head>
+<body>
+<h1>ORIENTAÇÃO A OBJETOS NO PHP OU OOP</h1>
 
-Objetos, são classes que criamos e nelas contém atributos e funções;
-Ex: classe Pessoa 
-Pode conter atributos como: Nome, Idade, Peso
-E Funções como: Crescer(), Comer(), Dormir()
+<p>Objetos, são classes que criamos e nelas contém atributos e funções;</p>
+<p>Ex: classe Pessoa</p> 
+<p>Pode conter atributos como: Nome, Idade, Peso</p>
+<p>E Funções como: Crescer(), Comer(), Dormir()</p>
+<p>Private: função que não pode ser chamada diretamente. Para evocar uma função privada pode se utilizar "$this->" + "nome da função" dentro de uma public function.</p>
+</body>
+</html>
 
 <?php 
     class Pessoa{
@@ -13,15 +24,22 @@ E Funções como: Crescer(), Comer(), Dormir()
         private $idade = '28';
         private $peso = '70 Kg';
 
-        private function crescer() {
-
+        public function crescer() {
+            echo 'estou crescendo';
+            $this->comer();
 
         }
 
         private function comer() {
-
+            echo 'estou comendo';
             
         }
     }
+
+    //Instanciar
+    $pessoa = new Pessoa;
+    $pessoa2 = new Pessoa;
+
+    $pessoa->crescer();
     
 ?>
