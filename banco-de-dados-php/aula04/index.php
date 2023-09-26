c875db9b290953f6ced741d959a805c6b23e996a
Lucas Fragalá <lucfragala@gmail.com>
2023-09-26 20:49:14 -0300

    aula04

---
+++ banco-de-dados-php/aula04/index.php
@@ -0,0 +1,10 @@
+<?php 
+    $pdo = new PDO('mysql:host=localhost;dbname=modulo_8','root','wab12345678');
+    //atualizando banco de dados por Query
+    $id = 17;
+    $sql = $pdo->prepare("DELETE FROM `clientes` WHERE id=?");
+
+    if($sql->execute(array($id))){
+        echo 'Meu cliente foi deletado com sucesso';
+    }
+?>
