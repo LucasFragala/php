66b7e065113b5d5df80c5db17538b4173d452a02
Lucas Fragalá <lucfragala@gmail.com>
2023-09-26 19:13:36 -0300

    aula09

---
+++ objeto-php/aula09/index.php
@@ -0,0 +1,12 @@
+<?php 
+    class minhaClasse{
+        const VALOR = 300;
+        function __construct(){
+            echo self::VALOR;
+        }
+    }
+
+    new minhaClasse;
+    echo minhaClasse::VALOR; //chamar de fora
+
+?>
