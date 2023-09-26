cc10105384139ca3e4621182610c79481642bb35
Lucas Fragalá <lucfragala@gmail.com>
2023-09-26 18:16:24 -0300

    aulal07

---
+++ objeto-php/aula07/class2.php
@@ -0,0 +1,10 @@
+<?php
+    
+
+    class Class2 {
+        function __construct() {
+            echo ' Class2 instanciada';
+        }
+    }
+
+?>
---
+++ objeto-php/aula07/index.php
@@ -0,0 +1,8 @@
+<h1>Namespace e use Keywords</h1>
+
+<?php 
+include('Class1.php');
+include('Class2.php');
+
+$class1 = new \Sessao1\Class1();
+?>
