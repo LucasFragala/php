0883b7eb0bc8257437a481865484c50cafbb6e41
Lucas Fragalá <lucfragala@gmail.com>
2023-09-28 16:49:20 -0300

    Update index2.php

--- banco-de-dados-php/aula05/index2.php
+++ banco-de-dados-php/aula05/index2.php
@@ -6,7 +6,7 @@
 
     $info = $sql->fetchAll(); //pegar todas as informações buscadas pela Querry.
 
-    //Puxar dado da tabela utilizando foreach
+    //Puxar os dados da tabela utilizando foreach
 
     foreach ($info as $key => $value){
         $categoria_id = $value['id'];
