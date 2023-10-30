c39f6a66b34ae1f30d41b6f82e90bb657f34fb48
Lucas Fragalá <lucfragala@gmail.com>
2023-10-30 16:55:18 -0300

    up

---
+++ objeto-php/aula00/Caneta.php
@@ -0,0 +1,26 @@
+<?php
+
+class Caneta
+{
+    var $modelo;
+    var $cor;
+    var $ponta;
+    var $carga;
+    var $tampada;
+
+    function rabiscar(){
+        if ($this->tampada) {
+            echo "<p>A caneta está tampada, não posso rabiscar!</p>";
+        } else {
+            echo "Estou rabiscando";
+        }
+    }
+    function tampar(){
+        $this->tampada = true;
+    }
+    function destampar(){
+        $this->tampada = false;
+    }
+}
+
+?>
---
+++ objeto-php/aula00/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Aula 00</title>
+</head>
+<body>
+    <?php
+        require_once 'Caneta.php';
+        $c1 = new Caneta;
+        $c1->cor = "Azul";
+        $c1->ponta = 0.5;
+        $c1->tampada = true;
+        $c1->destampar();
+        $c1->rabiscar();
+    ?>
+</body>
+</html>
